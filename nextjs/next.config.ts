9f5dfeed25116897817482cbafa611f23d182d30
Brando Endona <cbrandoendona@gmail.com>
2025-08-15 11:29:24 +0800

    update next.config

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -6,8 +6,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol:
-          (process.env.NEXT_PUBLIC_API_PROTOCOL as "http" | "https") || "http",
-        hostname: process.env.NEXT_PUBLIC_API_DOMAIN || "localhost",
+          (process.env.NEXT_PUBLIC_IMAGE_STORAGE_PROTOCOL as
+            | "http"
+            | "https") || "http",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_STORAGE_DOMAIN || "localhost",
         pathname: "/**",
       },
     ],
