655a48ae49531b47eeede9989c85057eb0b2444d
Brando Endona <cbrandoendona@gmail.com>
2025-08-15 10:59:27 +0800

    fix lint error in next.config.js

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol:
           (process.env.NEXT_PUBLIC_API_PROTOCOL as "http" | "https") || "http",
-        hostname: process.env.NEXT_PUBLIC_API_DOMAIN || "",
+        hostname: process.env.NEXT_PUBLIC_API_DOMAIN || "localhost",
         pathname: "/**",
       },
     ],
